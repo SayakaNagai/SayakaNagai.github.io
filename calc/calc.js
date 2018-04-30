2c87bc068b43080602d2ba5146e951cb9c0c13d2
SayakaNagai <1355059t@stu.kobe-u.ac.jp>
2018-04-30 22:17:01 +0900

    upload

--- calc/calc.js
+++ calc/calc.js
@@ -1,5 +1,15 @@
-function onButtonClick( str )
-{
-    document.write( eval(str) );
-    return eval(str);
-}
+function tbox1(){      
+      var str=document.js.txtb.value;
+      ans = eval(str);
+      if( !isNaN(ans) ){
+      document.js.ans.value=ans;
+      }else{
+      document.js.txtb.value="";
+      alert("数式を入力してください!");
+      }
+      }
+
+      function clr(){
+      document.js.txtb.value="";
+      document.js.ans.value="";
+      }
